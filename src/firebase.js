6e66589848f8da9d28b13aa62067cfeac83bae55
romi-devx <romi_maximus@hotmail.com>
2020-09-30 14:37:20 -0300

    firebase config details

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,7 @@
 import firebase from 'firebase';
 
 const fb = firebase.initializeApp({
-    apiKey: "AIzaSyAupAoxy3FTDAsSERlIaxMkeXzS2zsNG9o",
-    authDomain: "todo-app-firebase-db188.firebaseapp.com",
-    databaseURL: "https://todo-app-firebase-db188.firebaseio.com",
-    projectId: "todo-app-firebase-db188",
-    storageBucket: "todo-app-firebase-db188.appspot.com",
-    messagingSenderId: "170681073507",
-    appId: "1:170681073507:web:271633bec9236af6b45d56",
-    measurementId: "G-29X2PY22TH"
+    "config-Details":"enter your firebase config data here"
 });
 
 const db = fb.firestore();
